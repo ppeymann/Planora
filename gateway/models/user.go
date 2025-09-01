54e9fa0861ece5aec1bb9cbe6cc23acb9f6d562e
Peyman <p.malek32@gmail.com>
2025-09-01 21:11:30 +0330

    add all user services' file and add user model

---
+++ gateway/models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ppeymann/Planora.git/pkg/common"
+)
+
+type (
+	// UserService represents method signatures for api user endpoint.
+	// so any object that stratifying this interface can be used as user service for api endpoint.
+	UserService interface {
+		// SignUp service is for signing up user if never sign in
+		SignUp(ctx *gin.Context) *common.BaseResult
+	}
+
+	// UserHandler represents method signatures for user handlers.
+	// so any object that stratifying this interface can be used as user handlers.
+	UserHandler interface {
+		// SignUp handler is for signing up user if never sign in
+		SignUp(ctx *gin.Context)
+	}
+)
