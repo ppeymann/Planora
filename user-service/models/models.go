0b465d9b42345cd097a201e6f92af13096a3f3ee
Peyman <p.malek32@gmail.com>
2025-09-02 20:55:12 +0330

    create models for repository

---
+++ user-service/models/models.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"errors"
+	"time"
+
+	"gorm.io/gorm"
+
+	userpb "github.com/ppeymann/Planora.git/proto/user"
+)
+
+var (
+	ErrAccountExist error = errors.New("account with specified params already exists")
+)
+
+type (
+	EventType string
+
+	UserRepository interface {
+		Create(in *userpb.SignUpRequest) (*UserEntity, error)
+	}
+
+	UserEntity struct {
+		gorm.Model
+
+		Username  string `json:"username" gorm:"uniqueIndex;not null"`
+		Email     string `json:"email" gorm:"uniqueIndex;not null"`
+		Password  string `json:"-" gorm:"not null"`
+		FirstName string `json:"first_name" gorm:"column:first_name"`
+		LastName  string `json:"last_name" gorm:"column:last_name"`
+	}
+
+	RefreshTokenEntity struct {
+		gorm.Model
+		AccountID uint
+		TokenId   string `json:"token_id" gorm:"column:token_id;index"`
+		UserAgent string `json:"user_agent" gorm:"column:user_agent"`
+		IssuedAt  int64  `json:"issued_at" bson:"issued_at" gorm:"column:issued_at"`
+		ExpiredAt int64  `json:"expired_at" bson:"expired_at" gorm:"column:expired_at"`
+	}
+
+	TokenBundlerOutput struct {
+		// Token is string that hashed by paseto
+		Token string `json:"token"`
+
+		// Refresh is string that for refresh old token
+		Refresh string `json:"refresh"`
+
+		// Expire is time for expire token
+		Expire time.Time `json:"expire"`
+	}
+)
+
+const (
+	Create EventType = "user.CREATE"
+)
