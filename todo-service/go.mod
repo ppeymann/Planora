fd2095e001586b12d25a4f15cb36eb47cfe8d37a
Peyman <p.malek32@gmail.com>
2025-09-05 08:09:22 +0330

    create todo models

--- todo-service/go.mod
+++ todo-service/go.mod
@@ -1,3 +1,16 @@
 module github.com/ppeymann/Planora/todo
 
 go 1.24.2
+
+replace github.com/ppeymann/Planora.git => ../
+
+require (
+	github.com/ppeymann/Planora.git v0.0.0-00010101000000-000000000000
+	gorm.io/gorm v1.30.3
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.28.0 // indirect
+)
--- todo-service/models/models.go
+++ todo-service/models/models.go
@@ -1,1 +1,39 @@
 package models
+
+import (
+	"github.com/ppeymann/Planora.git/pkg/common"
+	"gorm.io/gorm"
+)
+
+type (
+	EventType string
+
+	StatusType string
+
+	TodoRepository interface {
+		Create() (*TodoEntity, error)
+
+		common.BaseRepository
+	}
+
+	TodoEntity struct {
+		gorm.Model
+
+		// Title
+		Title string `json:"title" gorm:"column:title;index;not null"`
+
+		// Description
+		Description string `json:"description" gorm:"column:description"`
+
+		// Status
+		Status StatusType `json:"status" gorm:"column:status;default:'DO';check:status IN ('DO','IN_PROGRESS','DONE','ARCHIVED','BLOCKED')"`
+	}
+)
+
+const (
+	StatusDo         StatusType = "DO"
+	StatusInProgress StatusType = "IN_PROGRESS"
+	StatusDone       StatusType = "DONE"
+	StatusArchived   StatusType = "ARCHIVED"
+	StatusBlocked    StatusType = "BLOCKED"
+)
